b2cedc601289eab2e23d613a2576c721a308f480
dipeshsukhani@gmail.com <dipeshsukhani@gmail.com>
2019-02-16 16:55:43 +0800

    Updated the gitignore to remove the db url

--- application.py
+++ application.py
@@ -1,5 +1,5 @@
 import os
-import hashlib
+import secret
 
 from flask import Flask, render_template, request
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -12,8 +12,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
-engine = create_engine(
-    "postgres://sqlxamuciiofie:86b317ef4be1dc74d800442ecdfe1447933e12298c36a5333d6a3fdfddc27b9d@ec2-54-83-50-174.compute-1.amazonaws.com:5432/d33ltdp29n43i3")
+
+engine = create_engine(secret.database_url)
 db = scoped_session(sessionmaker(bind=engine))
 
 app = Flask(__name__)
