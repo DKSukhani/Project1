c72bdb0652c359b42d6a97863508bba23c55fac4
dipeshsukhani@gmail.com <dipeshsukhani@gmail.com>
2019-02-19 15:09:21 +0800

    Login password check form.  It now works.

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ import os
 import secret
 
 from flask import Flask, render_template, request
-from werkzeug.security import generate_password_hash, check_password_hash
+
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
 
@@ -29,15 +29,44 @@ def index():
 def hello():
     email = request.form.get("email")
     password = request.form.get("password")
-    password = bcrypt.generate_password_hash(password)
+    print("password without the decode is "+ password)
+    password = bcrypt.generate_password_hash(password,10).decode('utf-8')
     db.execute("INSERT INTO users (email, password) VALUES (:email, :password)",
             {"email": email, "password": password})
     
     db.commit()
     data = db.execute(
-         "SELECT password FROM users").fetchall()
+         "SELECT email FROM users").fetchall()
     data = str(data)
     return(data)
+
+@app.route("/check", methods=["POST", "GET"])
+def check():
+        email = request.form.get("login_email")
+        check_email_in_db = db.execute("SELECT COUNT(*) FROM users WHERE email = :email", {"email": email}).fetchall()
+        if check_email_in_db[0][0] == 1 :
+            email = request.form.get("login_email")
+            password = request.form.get("login_password")
+            retrive_password_from_db = db.execute("SELECT password FROM users WHERE email = :email", {"email": email}).fetchall()
+            retrive_password_from_db = retrive_password_from_db[0][0]
+            if bcrypt.check_password_hash(retrive_password_from_db, password):
+                return("this works")
+            else:
+                return("something is wrong")
+        
+            
+            # if abc == password:
+            #    return("all is good")
+            # else:
+            #     return ("Sorry you have entered an incorrect password")
+            # rs = db.execute("SELECT password FROM users WHERE email = :email" , {"email": email, "password": password}).fetchall()
+            # if rs[0][0] == 1 :
+            #     return ("Thank you for logging-in")
+            # else:
+            #     
+        
+        else:
+            return ("Sorry, you are not a registered user")
     
     
 if __name__ == '__main__':
