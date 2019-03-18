00d998cf76a9d459d759a974249d99f6ee577312
dipeshsukhani@gmail.com <dipeshsukhani@gmail.com>
2019-03-18 10:52:46 +0800

    app.run

--- application.py
+++ application.py
@@ -82,6 +82,6 @@ def search_result():
 
 
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='dipesh-book-app.herokuapp.com', port=port)
+if __name__ == '__main__':
+        port = int(os.environ.get('PORT', 33507)) 
+        app.run(host='0.0.0.0', port=port)
